cf78a1e23f045aa2464e45c80613b53a9ca84f0a
gowri <vngowri@gmail.com>
2017-03-08 15:49:44 -0600

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3> About component</h3>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the About page!</p>
+    </div>
   );
 }
 module.exports = About;
